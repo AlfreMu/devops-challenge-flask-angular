07ae1ba5daef6ff9726e99d8faa3eade3067a8ba
AlfreMu <141455009+AlfreMu@users.noreply.github.com>
2026-02-04 01:06:02 -0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -29,4 +29,29 @@ resource "aws_security_group" "app_sg" {
   tags = {
     Name = "${var.project_name}-sg"
   }
+
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# Crear la instancia EC2
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id
+  ]
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
 }
