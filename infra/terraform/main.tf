33fed70f630f29487c4206f8aace0eea7385a38c
AlfreMu <141455009+AlfreMu@users.noreply.github.com>
2026-02-04 01:00:06 -0300

    Create main.tf

---
+++ infra/terraform/main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Security Group para la app (SSH + HTTP)"
+
+  ingress {
+    description = "SSH desde mi IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Salida a Internet"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
